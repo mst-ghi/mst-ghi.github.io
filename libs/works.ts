bdd826cc7c87e1fbf35a16b0cbc88169da3edbfa
Mostafa Gholami <mostafa.gholami@avidability.com>
2023-07-05 08:31:15 +0330

    works data updated

--- libs/works.ts
+++ libs/works.ts
@@ -5,8 +5,9 @@ const WorksData = [
     location: 'Yerevan, Armenia',
     date: 'Since October 2021',
     details: [
-      'Working with a team of twelve people and programmers',
-      'Development of a multitenant web app with Nextjs In workouts, advice, legal and etc. Project development uses Turborepo to create components as an internal package and handle multiple᛫ web apps in one source code',
+      'Working with a team of twelve programmers',
+      'Developing a multitenant web app with Next.js for workouts, advice, legal, etc',
+      'Project development utilizes Turborepo to create components as an internal package and handle multiple web apps in one source code',
       'Using the Mantine UI library and Tailwind CSS',
     ],
   },
@@ -16,11 +17,11 @@ const WorksData = [
     location: 'Tehran, Iran',
     date: 'December 2020 - February 2022',
     details: [
-      'Working with a team of five people and programmers',
-      'Get stock market information using the Crowler technique',
-      'Design and development of various financial charts',
-      'Backend with two Lumen apps and one Nestjs',
-      'Frontend with Nuxtjs',
+      'Working with a team of five programmers',
+      'Utilizing web scraping techniques to gather stock market information',
+      'Designing and developing various financial charts',
+      'Backend development using two Lumen apps and one Nest.js app',
+      'Frontend development using Nuxt.js',
     ],
   },
   {
@@ -30,10 +31,10 @@ const WorksData = [
     date: 'November 2019 - November 2020',
     details: [
       'Working on a comprehensive business system project',
-      'Working with a team of seven programmers',
-      'Three backend apps with Nestjs and one app with Laravel',
-      'We used Redis to communicate between apps',
-      'Backoffice web app with Nuxtjs, Customer web app with React Nextjs',
+      'Collaborating with a team of seven programmers',
+      'Developing three backend applications using Nest.js and one application using Laravel',
+      'Utilizing Redis for inter-app communication',
+      'Building the backoffice web application using Nuxt.js and the customer web application using React and Next.js',
     ],
   },
   {
@@ -44,7 +45,10 @@ const WorksData = [
     details: [
       'Working with a team of five programmers',
       'Development of several shopping and entertainment projects',
-      'Backend with Laravel and Nestjs, Frontend with Nuxtjs',
+      'Backend development using Laravel and Nest.js',
+      'Frontend development using Nuxt.js',
+      'Integration of payment gateway for secure transactions',
+      'Implementation of user authentication and authorization features',
     ],
   },
   {
@@ -55,7 +59,9 @@ const WorksData = [
     details: [
       'Development of the marketer management project',
       'Working with a team of eight programmers',
-      'Develop two backends app with Laravel (Lumen) and Nestjs, frontend with Nuxtjs',
+      'Developing two backend applications using Laravel (Lumen) and Nest.js',
+      'Frontend development using Nuxt.js',
+      'Implementation of a user-friendly dashboard for marketers',
     ],
   },
   {
@@ -64,9 +70,9 @@ const WorksData = [
     location: 'Isfahan, Iran',
     date: 'May 2016 - May 2017',
     details: [
-      'Develope websites with Laravel and Vuejs',
-      'Implemented front-end web design and worked with clients to collect performance feedback',
-      'Completed application and website designs within project scope and deadlines',
+      'Developing websites using Laravel and Vue.js',
+      'Implementing front-end web design and collaborating with clients to gather performance feedback',
+      'Delivering application and website designs within project scope and meeting deadlines',
     ],
   },
 ]

849ace7f2e8d763f5dc83c39d714b3357a8b66b2
Mostafa Gholami <mostafagholamidev@gmail.com>
2024-08-14 09:10:56 +0330

    source updated
    Signed-off-by: Mostafa Gholami <mostafagholamidev@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
+import { Lato } from 'next/font/google';
 import { AppHead } from '@/components/shell';
 import AppProvider from '@/components/app-provider';
+
+const lato = Lato({
+  weight: '700',
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className={lato.className}>
       <AppHead />
       <body suppressHydrationWarning>
         <AppProvider>{children}</AppProvider>

872f0a7a586fcb5e3c3949d06b47b741b37fa680
Mostafa Gholami <mostafagholamidev@gmail.com>
2025-02-21 07:42:29 +0330

    dependencies updated
    Signed-off-by: Mostafa Gholami <mostafagholamidev@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,11 @@ import { Fira_Code } from 'next/font/google';
 import { AppHead } from '@/components/shell';
 import AppProvider from '@/components/app-provider';
 
-const Fira = Fira_Code({
-  subsets: ['latin'],
-});
+const Fira = Fira_Code({ subsets: ['latin'] });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <AppHead />
       <body suppressHydrationWarning className={Fira.className}>
         <AppProvider>{children}</AppProvider>

bd31d552c033d547de554431dcf6f9cea8f503ff
Mostafa Gholami <mostafagholamidev@gmail.com>
2025-11-16 07:20:21 +0330

    dependencies updated to latest
    Signed-off-by: Mostafa Gholami <mostafagholamidev@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
+	output: 'export',
+	devIndicators: false,
+	reactStrictMode: true,
 };
 
 export default nextConfig;
